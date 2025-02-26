7244b68f38e1c5b36bfe7db37533b27303527e7e
icedevil777 <icedevil777111@mail.ru>
2025-02-26 13:17:51 +0300

    # add pagination

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,36 +1,69 @@
 <template>
-  <div class="sm:px-5">
+  <div class="sm:px-5 w-full">
     <h1 class="px-5 text-5xl font-normal">Articles</h1>
-    <div class="flex flex-wrap mt-5">
-      <div class="card w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 p-3" v-for="post in pugData" :key="post">
-        <img class="w-full rounded-lg" src="~/assets/img/post-img.png" />
-        <p class="mt-2 text-center text-lg md:text-xl">{{ post.title }}</p>
+    <div v-if="posts.length > 0" class="flex flex-wrap mt-5">
+      <div class="card w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 p-3" v-for="post in pagData" :key="post">
+        <NuxtLink :to="`/article/${post.id}`">
+          <img class="w-full rounded-lg" src="~/assets/img/post-img.png" />
+          <p class="mt-2 text-center text-lg md:text-xl">{{ post.id - 1 }} {{ post.title }}</p>
+        </NuxtLink>
       </div>
     </div>
-    <!-- <NuxtLink to="/article/1">Article page</NuxtLink> -->
+    <div v-else class="flex text-3xl w-[1400px] h-96 justify-content-center">
+      <span class="m-auto">Loading ...</span>
+    </div>
+    <div class="flex mt-5 pl-3 w-full">
+      <div class="flex mx-auto space-x-2">
+        <div v-if="currentPage > 0" @click="currentPage--" class="flex w-12 h-12 rounded-xl cursor-pointer pug-border">
+          <span class="m-auto text-xl"><</span>
+        </div>
+        <div
+          v-for="i in pagArray"
+          :key="i"
+          class="flex w-12 h-12 rounded-xl cursor-pointer"
+          :class="[i === currentPage + 1 ? 'bg-[black] text-white' : 'bg-[#F3F3F3] text-black']"
+          @click="setpagData(i - 1)"
+        >
+          <span class="m-auto text-lg">{{ i }}</span>
+        </div>
+        <div
+          v-if="currentPage < pagArray - 1"
+          @click="currentPage++"
+          class="flex w-12 h-12 rounded-xl cursor-pointer pug-border"
+        >
+          <span class="m-auto text-xl">></span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { getPosts } from '~/common/loadData';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 
 const { posts, loading, error, fetchPosts } = getPosts();
-const pugData = ref<Array<any>>([]);
+const pagData = ref<Array<any>>([]);
+const pagConst = <number>8;
+const currentPage = ref<number>(0);
 
-function setPugData(from: number): void {
-  pugData.value = posts.value.slice(from, from + 8);
+function setpagData(page: number): void {
+  pagData.value = posts.value.slice(page * pagConst, page * pagConst + pagConst);
+  currentPage.value = page;
 }
+
+const pagArray = computed(() => {
+  return Math.ceil(posts.value.length / pagConst);
+});
 
 onMounted(async () => {
   await fetchPosts();
-  setPugData(0);
-  console.log('pugData', pugData);
+  setpagData(0);
 });
 </script>
 
 <style scoped>
-.card {
-  width: calc();
+.pug-border {
+  border: solid black 1px;
 }
 </style>
